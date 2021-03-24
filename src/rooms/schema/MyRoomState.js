06954fa351aabc4e20d5d129c958c2bb96027c56
Frank Matranga <thefrankmatranga@gmail.com>
2021-03-24 11:45:08 -0400

    Basic room schema

--- src/rooms/schema/MyRoomState.js
+++ src/rooms/schema/MyRoomState.js
@@ -10,7 +10,22 @@ class MyRoomState extends schema.Schema {
 }
 
 schema.defineTypes(MyRoomState, {
-  players: [PlayerState]
+  /**
+   * The ordered list of players in the game room.
+   * Imagine this as a cycle, so if you want the
+   * next person after the last,
+   * you go back to the first person.
+   **/
+  players: [PlayerState],
+  /**
+   * The room's invite code that players can join with.
+   */
+  roomCode: 'string',
+  /**
+   * Should the room be publicly listed on the homepage?
+   * If false, players can only join by entering the room code.
+   */
+  isPublic: 'boolean'
 })
 
 exports.MyRoomState = MyRoomState
