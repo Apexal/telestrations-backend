434ef127eb0a56245e3a18e0babce0713af8d37b
Frank Matranga <thefrankmatranga@gmail.com>
2021-03-29 19:58:42 -0400

    Create StrokeState

---
+++ src/rooms/schema/StrokeState.js
@@ -0,0 +1,23 @@
+const schema = require('@colyseus/schema')
+
+/**
+ * Represents a single stroke of a drawing.
+ */
+class StrokeState extends schema.Schema {}
+
+schema.defineTypes(StrokeState, {
+  /** The x coordinate of the start of the stroke. */
+  fromX: 'number',
+  /** The y coordinate of the start of the stroke. */
+  fromY: 'number',
+  /** The x coordinate of the end of the stroke. */
+  toX: 'number',
+  /** The y coordinate of the end of the stroke. */
+  toY: 'number',
+  /** The width of the stroke. */
+  width: 'uint8',
+  /** The color of the stroke. */
+  color: 'string'
+})
+
+exports.StrokeState = StrokeState
