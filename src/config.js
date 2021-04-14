279d12a919d07d49f14d7efe6d0f7e15a2de81be
Frank Matranga <thefrankmatranga@gmail.com>
2021-04-14 12:34:36 -0400

    Document config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,13 @@
 module.exports = {
+  /** Rules for display names. */
   displayNames: {
     minLength: 3,
     maxLength: 20
   },
+  /** The length of the randomly generated room ids. */
   roomIdLength: 5,
+  /** The minimum number of players needed before a game can start. */
+  minClients: 3,
+  /** The maximum number of players a game can hold. */
   maxClients: 12
 }
